a388662115c4e3ae080d1a83d80a3d86182e9cde
javicaylent <javier@caylent.com>
2021-06-14 10:34:44 -0500

    Update docker-build.sh

--- cli/docker-build.sh
+++ cli/docker-build.sh
@@ -2,7 +2,7 @@
 
 set +xe
 
-DOCKER_SLUG_REPO="javiortizmol"
+DOCKER_SLUG_REPO=""
 DOCKER_IMAGE_NAME="aws_sso_magic"
 DOCKER_REPO_REMOTE="$DOCKER_SLUG_REPO/$DOCKER_IMAGE_NAME"
 VERSION_FILE_PATH="src/aws_sso_magic/__init__.py"
@@ -19,8 +19,10 @@ docker build -t $DOCKER_IMAGE_NAME .
 docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:$VERSION >/dev/null
 docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:latest >/dev/null
 
-docker tag $DOCKER_IMAGE_NAME $DOCKER_REPO_REMOTE:$VERSION >/dev/null
-docker tag $DOCKER_IMAGE_NAME $DOCKER_REPO_REMOTE:latest >/dev/null
+if [ "$DOCKER_SLUG_REPO" != "" ]; then
+    docker tag $DOCKER_IMAGE_NAME $DOCKER_REPO_REMOTE:$VERSION >/dev/null
+    docker tag $DOCKER_IMAGE_NAME $DOCKER_REPO_REMOTE:latest >/dev/null
 
-docker push $DOCKER_REPO_REMOTE:latest
-docker push $DOCKER_REPO_REMOTE:$VERSION
+    docker push $DOCKER_REPO_REMOTE:latest
+    docker push $DOCKER_REPO_REMOTE:$VERSION
+fi
