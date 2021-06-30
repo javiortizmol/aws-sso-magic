96c59732dfd81b6cac250a3c1d812d187ffda1d3
javicaylent <javier@caylent.com>
2021-06-29 20:13:56 -0500

    Update eks.py

--- src/aws_sso_magic/eks.py
+++ src/aws_sso_magic/eks.py
@@ -97,7 +97,7 @@ def _eks_print_instructions(profile_name):
     print("\nPowerShell:")
     print(f"$Env:AWS_PROFILE={profile_name}")
     print("aws sts get-caller-identity\n")
-    print("\nNOTE: If you will select another profile, please first unset the AWS_PROFILE environment variable")
+    print("\nNOTE: If you will select another profile, please first unset the AWS_PROFILE environment variable or close this terminal and open a new one")
 
 def _eks_cluster_configuration():
     _check_kubectl()
